729001c276ba5d7efa8f36987cff3e212d78b60e
oluwayelu <oluwayeluifeoluwa@gmail.com>
2022-11-17 23:42:24 +0100

    fix: icons color to affect component

--- src/components/icons/ZuDown.tsx
+++ src/components/icons/ZuDown.tsx
@@ -19,7 +19,7 @@ const SvgZuDown = ({ size, color, className, ...props }: Props) => {
     >
       <path
         d="M6.883.006H.7a.562.562 0 0 0-.396.96L3.01 3.671a1.112 1.112 0 0 0 1.572 0l1.028-1.029L7.285.966c.35-.355.099-.96-.402-.96Z"
-        fill="#fff"
+        fill="currentColor"
       />
     </svg>
   );
--- src/components/icons/ZuEmail.tsx
+++ src/components/icons/ZuEmail.tsx
@@ -17,13 +17,13 @@ const SvgZuEmail = ({ size, color, className, ...props }: Props) => {
       preserveAspectRatio="xMidYMid meet"
       {...props}
     >
-      <g clipPath="url(#zu_email_svg__a)" fill="#fff">
+      <g clipPath="url(#zu_email_svg__a)" fill="currentColor">
         <path d="M16.322 2.766H3.112A1.834 1.834 0 0 0 1.279 4.6v1.468a.367.367 0 0 0 .191.322l8.073 4.403a.367.367 0 0 0 .351 0l8.072-4.403a.368.368 0 0 0 .191-.322V4.6a1.834 1.834 0 0 0-1.834-1.834Z" />
         <path d="M10.245 11.438a1.1 1.1 0 0 1-1.053 0L1.286 7.127l-.009.005v7.742a1.834 1.834 0 0 0 1.835 1.835h13.209a1.834 1.834 0 0 0 1.834-1.835V7.132l-.008-.005-7.903 4.31Z" />
       </g>
       <defs>
         <clipPath id="zu_email_svg__a">
-          <path fill="#fff" transform="translate(.911 .931)" d="M0 0h17.612v17.612H0z" />
+          <path fill="currentColor" transform="translate(.911 .931)" d="M0 0h17.612v17.612H0z" />
         </clipPath>
       </defs>
     </svg>
