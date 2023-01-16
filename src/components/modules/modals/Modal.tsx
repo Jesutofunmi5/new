154cad60556e7834a509800a8b0cfd4022ece24e
Anthony Agwa <agwaanthony63@gmail.com>
2023-01-16 13:38:50 +0100

    feat: initial commit

--- src/components/modules/modals/Modal.tsx
+++ src/components/modules/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
 const Modal = ({ size, content, title, setOpenModal, ...otherProps }: ModalProps) => {
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-70 z-50 flex items-center justify-center p-5"
+      className="fixed h-[700px] inset-0 bg-black bg-opacity-70 z-50 flex items-center justify-center p-5"
       {...otherProps}
     >
       <div className={`${size} bg-white rounded-lg`}>
