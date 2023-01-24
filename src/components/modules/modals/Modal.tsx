39a18f34fd42c9e6d9ea1b43908849e34fc20298
oluwayelu <oluwayeluifeoluwa@gmail.com>
2023-01-24 22:59:35 +0100

    fix: scroll on modal component

--- src/components/modules/modals/Modal.tsx
+++ src/components/modules/modals/Modal.tsx
@@ -1,34 +1,48 @@
-import { close } from "assets/images";
-import React from "react";
+import { useEffect } from "react";
+import { ZuClose } from "components/icons";
+
+import type { ReactNode } from "react";
 
 type ModalId = string | null;
 
 interface ModalProps {
   size: string;
   title?: string;
-  content: React.ReactNode;
+  content: ReactNode;
   setOpenModal: (value: ModalId | null) => void;
   [key: string]: any;
 }
 
 const Modal = ({ size, content, title, setOpenModal, ...otherProps }: ModalProps) => {
+  useEffect(() => {
+    const scrollY = window.scrollY;
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${scrollY}px`;
+
+    return () => {
+      document.body.style.position = "";
+      document.body.style.top = "";
+      window.scrollTo(0, scrollY);
+    };
+  }, [window.scrollY]);
+
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-70 z-50 flex items-center justify-center p-5"
+      className="w-full h-screen overflow-x-hidden fixed inset-0 bg-black bg-opacity-70 z-50 flex items-center justify-center p-5"
       {...otherProps}
     >
-      <div className={`${size} bg-white rounded-lg`}>
-        <div className="flex items-center justify-between px-4 py-2 border-b-2 border-b-gray-200 bg-gray-50 rounded-t-lg">
+      <div className={`relative w-full md:w-1/2 bg-white rounded-lg overflow-hidden`}>
+        <div className="w-full h-14 flex items-center justify-between px-4 py-2 border-b-2 border-b-gray-200 bg-gray-50 rounded-t-lg">
           <p className="text-black">{title}</p>
-          <button
+          <ZuClose
+            size={12}
             onClick={() => {
               setOpenModal(null);
             }}
-          >
-            <img src={close} />
-          </button>
+            className="text-green cursor-pointer"
+          />
         </div>
-        <div className="p-2">{content}</div>
+        <div className="p-2 w-full min-h-[40vh] max-h-[80vh] overflow-y-auto">{content}</div>
       </div>
     </div>
   );
