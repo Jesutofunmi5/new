0abb03e425baa7dc18e671f6a05ec5c604e8586c
oluwayelu <oluwayeluifeoluwa@gmail.com>
2023-01-24 23:04:16 +0100

    refactor: live chat ui

--- src/components/modules/forms/LiveChat.tsx
+++ src/components/modules/forms/LiveChat.tsx
@@ -1,4 +1,5 @@
-import { send, user } from "assets/images";
+import { user } from "assets/images";
+import { ZuSend } from "components/icons";
 import React, { useState } from "react";
 
 const Chat = () => {
@@ -16,30 +17,30 @@ const Chat = () => {
   }
 
   return (
-    <div className="px-2 py-4 flex flex-col justify-between">
-      <ul className="h-[450px] flex flex-col gap-2 overflow-y-scroll ">
+    <div className="w-full h-full flex flex-col justify-between">
+      <div className="w-full h-[50vh] flex flex-col gap-2 overflow-y-auto snap-end">
         {messages.map((message) => (
-          <li key={message} className=" w-[80%]  flex gap-2">
+          <li key={message} className=" w-4/5 flex gap-2">
             <span>
               <img src={user} className="h-8 w-18" />
             </span>
-            <span className="rounded-r-md rounded-bl-md text-[24px] bg-gray-50 p-2">{message}</span>
+            <span className="rounded-r-md rounded-bl-md text-lg bg-gray-50 p-2">{message}</span>
           </li>
         ))}
-      </ul>
-      <form onSubmit={handleSendMessage} className="flex items-center gap-2 pt-4">
+      </div>
+      <form onSubmit={handleSendMessage} className="w-full flex items-center gap-2 pt-4">
         <input
           type="text"
           value={input}
           onChange={handleInputChange}
           placeholder="Send Message..."
-          className="bg-white p-2 w-[80%] border-2 border-gray-100 rounded-md"
+          className="bg-white p-2 w-full border-2 border-gray-100 rounded-md"
         />
         <button type="submit" className="bg-green p-3 rounded-full text-white">
           <img />
         </button>
         <button type="submit" className="bg-green p-3 rounded-full text-white">
-          <img src={send} />
+          <ZuSend />
         </button>
       </form>
     </div>
