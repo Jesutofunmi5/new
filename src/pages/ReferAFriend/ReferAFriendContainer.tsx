b2490c4273f9c771bf10663dfc6ad0407861e371
Tamaraebi Dogubo <tdogubo@gmail.com>
2023-01-24 12:32:37 +0100

    feat(api): referral api integration

--- src/pages/ReferAFriend/ReferAFriendContainer.tsx
+++ src/pages/ReferAFriend/ReferAFriendContainer.tsx
@@ -1,15 +1,17 @@
 import { Dashboard } from "components";
 import ReferAFriendView from "./ReferAFriendView";
+import { userData } from "services/users.service";
 
 export const ReferAFriendContainer = () => {
   const referralMessage = `Achieve more with the Zumaridi Video Conferencing!
   Trust me, you won't regret it!`;
+  const refId = userData?.refId as string;
+
+  const url = window.location.href + "/" + refId;
+
   return (
     <Dashboard>
-      <ReferAFriendView
-        referral="https://www.zumaridi.com/invites/11AA232aq33"
-        title={referralMessage}
-      />
+      <ReferAFriendView referral={url} title={referralMessage} />
     </Dashboard>
   );
 };
