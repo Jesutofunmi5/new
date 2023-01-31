6b6dfaff6e71d660b5c20bd1731e7cc27730b692
eyong-joel7 <eyong.joel@yahoo.com>
2023-01-31 23:09:17 +0200

    fix: technical support service opt

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -2,15 +2,10 @@ import { TECHNICAL_SUPPORT, PUBLIC_REQUEST_KEY } from "./CONSTANTS";
 import fetchInstance from "./utils/FetchInterceptor";
 
 export async function requestTechnicalSupport(supportParams: FormData) {
-  try {
-    const response = await fetchInstance({
-      url: TECHNICAL_SUPPORT,
-      method: "post",
-      data: supportParams,
-      headers: { "Content-Type": "multipart/form-data", [PUBLIC_REQUEST_KEY]: true }
-    });
-    return response;
-  } catch (err) {
-    return err;
-  }
+  return await fetchInstance({
+    url: TECHNICAL_SUPPORT,
+    method: "post",
+    data: supportParams,
+    headers: { "Content-Type": "multipart/form-data", [PUBLIC_REQUEST_KEY]: true }
+  });
 }
